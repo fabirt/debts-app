bb51789f87e24a6cdc44a89f1c813f8cbcd6540e
Fabian Diartt <fdiartt@technisupport.com>
2020-02-08 16:37:38 -0500

    Migrando a androidX. Actualizando dependencias

--- lib/src/bloc/inherited_bloc.dart
+++ lib/src/bloc/inherited_bloc.dart
@@ -3,30 +3,27 @@ import 'package:debts_app/src/bloc/debtors_bloc.dart';
 import 'package:debts_app/src/bloc/lenders_bloc.dart';
 
 class InheritedBloc extends InheritedWidget {
-
   static InheritedBloc _instance;
 
   final DebtorsBloc debtorsBloc = new DebtorsBloc();
   final LendersBloc lendersBloc = new LendersBloc();
 
-  factory InheritedBloc({ Key key, Widget child }) {
+  factory InheritedBloc({Key key, Widget child}) {
     if (_instance == null) {
       _instance = new InheritedBloc._internal(key: key, child: child);
     }
     return _instance;
   }
 
-  InheritedBloc._internal({ Key key, Widget child })
-    : super(key: key, child: child);
-
+  InheritedBloc._internal({Key key, Widget child})
+      : super(key: key, child: child);
 
   @override
   bool updateShouldNotify(InheritedWidget oldWidget) {
     return true;
   }
 
-  static InheritedBloc of (BuildContext context) {
-    return ( context.inheritFromWidgetOfExactType(InheritedBloc) as InheritedBloc );
+  static InheritedBloc of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<InheritedBloc>();
   }
-
 }
