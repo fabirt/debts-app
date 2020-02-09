e8cfb2d0c601b15923dc8c6beac78353bd48358b
Fabian Diartt <fdiartt@technisupport.com>
2020-02-09 13:56:06 -0500

    Agregando flutter localizations

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
 import 'package:debts_app/src/pages/index.dart';
 import 'package:debts_app/src/widgets/index.dart';
 import 'package:debts_app/src/bloc/inherited_bloc.dart';
@@ -6,9 +8,36 @@ import 'package:debts_app/src/utils/index.dart' as utils;
 
 void main() {
   runApp(MyApp());
-} 
- 
+}
+
 class MyApp extends StatelessWidget {
+  final _supportedLocales = <Locale>[
+    const Locale('es'),
+  ];
+
+  Locale _localeResolutionCallback(
+    Locale locale,
+    Iterable<Locale> supportedLocales,
+  ) {
+    if (locale == null) {
+      return supportedLocales.first;
+    } else {
+      for (final supportedLocale in supportedLocales) {
+        if (supportedLocale.languageCode == locale.languageCode) {
+          return supportedLocale;
+        }
+      }
+      return supportedLocales.first;
+    }
+  }
+
+  Widget _builder(BuildContext context, Widget child) {
+    return ScrollConfiguration(
+      behavior: NeverOverScrollBehavior(),
+      child: child,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return InheritedBloc(
@@ -22,15 +51,16 @@ class MyApp extends StatelessWidget {
           primarySwatch: utils.Swatchs.greenSwatch,
           scaffoldBackgroundColor: utils.Colors.athensGray,
           textTheme: TextTheme(
-            body1: TextStyle(color: utils.Colors.brightGray)
+            body1: TextStyle(color: utils.Colors.brightGray),
           ),
         ),
-        builder: (BuildContext context, child) {
-          return ScrollConfiguration(
-            behavior: NeverOverScrollBehavior(),
-            child: child,
-          );
-        },
+        supportedLocales: _supportedLocales,
+        localeResolutionCallback: _localeResolutionCallback,
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+        ],
+        builder: _builder,
       ),
     );
   }
