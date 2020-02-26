a7ab647283ee30c74b196c69fa0e2370fac5ee1b
Fabian Diartt <fdiartt@technisupport.com>
2020-02-26 12:10:09 -0500

    Renombrando archivos de la carpeta models

--- lib/core/data/models/index.dart
+++ lib/core/data/models/index.dart
@@ -1,5 +1,5 @@
-export 'package:debts_app/core/data/models/debtor.dart';
-export 'package:debts_app/core/data/models/debt.dart';
-export 'package:debts_app/core/data/models/debtors_resume.dart';
-export 'package:debts_app/core/data/models/lender.dart';
-export 'package:debts_app/core/data/models/loan.dart';
+export 'package:debts_app/core/data/models/debtor_model.dart';
+export 'package:debts_app/core/data/models/debt_model.dart';
+export 'package:debts_app/core/data/models/debtors_resume_model.dart';
+export 'package:debts_app/core/data/models/lender_model.dart';
+export 'package:debts_app/core/data/models/loan_model.dart';
